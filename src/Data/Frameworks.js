67a509870f693c60476f43eed90573282c6ae13e
unknown <l.boukorras@gmail.com>
2021-11-15 13:57:05 +0100

    install Dependencie,Asset and Data

---
+++ src/Data/Frameworks.js
@@ -0,0 +1,22 @@
+export const Frameworks = [
+  {
+    id: 1,
+    value: "React",
+    xp: 1.5,
+  },
+  {
+    id: 2,
+    value: "React Native",
+    xp: 1.5,
+  },
+  {
+    id: 3,
+    value: "Sass",
+    xp: 1.5,
+  },
+  {
+    id: 4,
+    value: "Node.js",
+    xp: 1.5,
+  },
+];
---
+++ src/Data/Languages.js
@@ -0,0 +1,12 @@
+export const Languages = [
+  {
+    id: 1,
+    value: "Javascript",
+    xp: 1.5,
+  },
+  {
+    id: 2,
+    value: "Css",
+    xp: 1.5,
+  },
+];
