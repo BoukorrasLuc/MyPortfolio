39105a9848791c34182bcb2fbae82b65ffd385a0
unknown <l.boukorras@gmail.com>
2022-05-29 13:37:02 +0200

    feat : add of languages,tools and project

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,85 +1,104 @@
 const Logo = () => {
   return (
     <div>
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Javascript_dm6hg3.svg"
-        alt="Logo Javascript"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/React_pkhicq.svg"
-        alt="Logo React"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Node_e6jl9p.svg"
-        alt="Logo Node"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Html_vu9pzw.svg"
-        alt="Logo Html"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209794/Svg/Yarn_gbjudg.svg"
-        alt="Logo Yarn"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209794/Svg/Github_w21zgh.svg"
-        alt="Logo Github"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209793/Svg/Git_ahxily.svg"
-        alt="Logo Git"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Npm_ui5eag.svg"
-        alt="Logo Npm"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Heroku_h6lxh0.svg"
-        alt="Logo Heroku"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Netlify_akakgo.svg"
-        alt="Logo Netlify"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Postman_qlsbgn.svg"
-        alt="Logo Postman"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1638102367/Svg/Firebase_w21zgh_luv9jt.svg"
-        alt="Logo Firebase"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Expo_rldgyw.svg"
-        alt="Logo Expo"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/mongodb_ly933d.svg"
-        alt="Logo mongodb"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Sass_m8gxei.svg"
-        alt="Logo Sass"
-      />
-      &ensp;
-      <img
-        src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209794/Svg/Css_e1tghb.svg"
-        alt="Logo Css"
-      />
+      <div>
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Javascript_dm6hg3.svg"
+          alt="Logo Javascript"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/React_pkhicq.svg"
+          alt="Logo React"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Node_e6jl9p.svg"
+          alt="Logo Node"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Html_vu9pzw.svg"
+          alt="Logo Html"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209794/Svg/Yarn_gbjudg.svg"
+          alt="Logo Yarn"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209794/Svg/Github_w21zgh.svg"
+          alt="Logo Github"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209793/Svg/Git_ahxily.svg"
+          alt="Logo Git"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Npm_ui5eag.svg"
+          alt="Logo Npm"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Heroku_h6lxh0.svg"
+          alt="Logo Heroku"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Netlify_akakgo.svg"
+          alt="Logo Netlify"
+        />
+        &ensp;
+      </div>
+      <div>
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Postman_qlsbgn.svg"
+          alt="Logo Postman"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1638102367/Svg/Firebase_w21zgh_luv9jt.svg"
+          alt="Logo Firebase"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/Expo_rldgyw.svg"
+          alt="Logo Expo"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631218573/Svg/mongodb_ly933d.svg"
+          alt="Logo Mongodb"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209792/Svg/Sass_m8gxei.svg"
+          alt="Logo Sass"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1631209794/Svg/Css_e1tghb.svg"
+          alt="Logo Css"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1653822022/Svg/TypeScript_qrkkou.svg"
+          alt="Logo TypeScript"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1653822023/Svg/GraphQl_arz4c8.svg"
+          alt="Logo GraphQl"
+        />
+        &ensp;
+        <img
+          src="https://res.cloudinary.com/dgu3expdz/image/upload/v1653822022/Svg/Strapi_z1ilpq.svg"
+          alt="Logo Strapi"
+        />
+      </div>
     </div>
   );
 };
